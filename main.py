a9b8ac9ae2895f0889f911e37e261d5ae86095d3
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-04 13:13:41 -0700

    Milo Dominugez

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ if len(num1) > len(num2):
 # ******************************
 
 # print ('True') or ('False')
+for i in range(len(num2)):
+	j = 0
+	count = 0
+	if num1[j] == num2[i]:
+		while j < len(num1) and num1[j] == num2[i] and count < len(num1):
+			i += 1
+			j += 1
+			count += 1
+	if count == len(num1):
+		break
+if count == len(num1):
+	print('True')
+else:
+	print('False')
